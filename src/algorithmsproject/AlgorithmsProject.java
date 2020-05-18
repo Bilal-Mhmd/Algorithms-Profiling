bf2d3ce62bd26daf06899b81fcf258b9a461df83
Bilal <Bilal@DESKTOP-7VFU8EA>
2020-05-18 07:31:12 +0300

    Final Commit

--- src/algorithmsproject/AlgorithmsProject.java
+++ src/algorithmsproject/AlgorithmsProject.java
@@ -5,49 +5,52 @@ package algorithmsproject;
  * @author Bilal & Amro
  */
 import java.util.Random;
+import java.util.ArrayList;
 
 public class AlgorithmsProject {
-
+        
     
+static ArrayList<Long> insertionList = new ArrayList<>();//TO RECORED DATA FOR INSERTION
+static ArrayList<Long> mergeList = new ArrayList<>();//TO RECORED DATA FOR MERGE
+static ArrayList<Long> quickList = new ArrayList<>();//TO RECORED DATA FOR QUICK
+
     public static void main(String[] args) {
-
-        System.out.println("for array of size 10k:");
-        pass(10000);
-         System.out.println("*****************************");
-        System.out.println("for array of size 20k:");
-        pass(20000);
-          System.out.println("*****************************");
-        System.out.println("for array of size 30k:");
-        pass(30000);
-           System.out.println("*****************************");
-        System.out.println("for array of size 40k:");
-        pass(40000);
-           System.out.println("*****************************");
-        System.out.println("for array of size 50k:");
-        pass(50000);
-           System.out.println("*****************************");
-        System.out.println("for array of size 60k:");
-        pass(60000);
-           System.out.println("*****************************");
-        System.out.println("for array of size 70k:");
-        pass(70000);
-           System.out.println("*****************************");
-        System.out.println("for array of size 80k:");
-        pass(80000);
-           System.out.println("*****************************");
-        System.out.println("for array of size 90k:");
-        pass(90000);
-           System.out.println("*****************************");
-        System.out.println("for array of size 100k:");
-        pass(100000);
-           System.out.println("*****************************");
-
+        int[] inputSize={100000,300000,500000,700000,900000,1100000,1300000,1500000,1700000,1900000,};
+        for (int i = 0; i < inputSize.length; i++) {
+             System.out.println("for array of size "+inputSize[i]);
+             pass(inputSize[i]);
+             System.out.println("************************************************");
+            
+        }
+        
+       //result list 
+        System.out.println("The result running time (in milli second) for the size inputs: \n100k,300k,300k,500k,700k,900k,1.1M, 1.3M, 1.5M, 1.7M, 1.9M");
+        System.out.print("Insertion Sort: ");
+        for(Long a: insertionList )
+        {
+            System.out.print( a +" ");
+        }
+         System.out.print("\nMerge Sort: ");
+         
+         for(Long b: mergeList )
+        {
+            System.out.print( b +" ");
+        }
+          System.out.print("\nQuick Sort: ");
+         for(Long c: quickList )
+        {
+            System.out.print( c +" ");
+        }
+         System.out.println("");
+        
+        
         //   printArray(arr);        
     }
 
     public static void calculateTime(int arr[]) {
+      
         int temp[] = new int[arr.length];
-
+        long timeDifference;
         for (int i = 0; i < arr.length; ++i) {
             temp[i] = arr[i];
         }
@@ -58,7 +61,9 @@ public class AlgorithmsProject {
         InsertionSort ob = new InsertionSort();
         ob.sort(temp);
         long end = System.currentTimeMillis();
-        System.out.println("Insertion Sort = :" + (end - start) + " milli second");
+        timeDifference = end - start;
+        insertionList.add(timeDifference);
+        System.out.println("Insertion Sort = :" +timeDifference + " milli second");
 
         for (int i = 0; i < arr.length; ++i) {
             temp[i] = arr[i];
@@ -68,18 +73,23 @@ public class AlgorithmsProject {
         MergeSort object = new MergeSort();
         object.sort(temp, 0, arr.length - 1);
         end = System.currentTimeMillis();
-        System.out.println("Merge Sort = :" + (end - start) + " milli second");
+        timeDifference = end - start;
+        mergeList.add(timeDifference);
+        System.out.println("Merge Sort = :" + timeDifference + " milli second");
 
         for (int i = 0; i < arr.length; ++i) {
             temp[i] = arr[i];
         }
+        
         //Quick Sort
 
         start = System.currentTimeMillis();
         QuickSort obj = new QuickSort();
         obj.sort(temp, 0, arr.length - 1);
         end = System.currentTimeMillis();
-        System.out.println("Quick Sort = :" + (end - start) + " milli second");
+        timeDifference = end - start;
+        quickList.add(timeDifference);
+        System.out.println("Quick Sort = :" + timeDifference + " milli second");
 
     }
 
@@ -92,7 +102,7 @@ public class AlgorithmsProject {
         calculateTime(arr);
 
     }
-    
+
     static void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; ++i) {
